91fb706273e85cb00684ecb431f8e7c35fde4424
Pete Melgren <pmelgren@gmail.com>
2019-01-21 10:58:15 -0500

    Move code to prepare dataframe over to the prepare_odds_data.R file. preserve first draft of join_table.csv save the data as csv's to cut down on time to read from online

--- landau.R
+++ landau.R
@@ -1,35 +1,11 @@
 library(fuzzyjoin)
 library(data.table)
 
-#read in the scores dataset from data world 
-scores = read.csv("https://query.data.world/s/ifvsddtb76qbf2i7f4ezmv6uutqtnj"
-                    ,header=TRUE ,stringsAsFactors=FALSE)
-scores = data.table(scores)
+# read in the 3 tables stored the data folder (after they have been prepared
+# by prepare_data.R)
+scores = data.table(read.csv("./scores.csv"))
+confs = data.table(read.csv("./confs.csv"))
+odds = data.table(read.csv("./odds.csv"))
+master = data.table(read.csv("./join_table.csv"))
 
-#filter by strategy criteria
-scores = scores[!is.na(Rk..1) & is.na(Rk.)
-                ,c("Year","Date","Schl","Rk.","Opp","Rk..1","PTS","OPP","MOV")]
 
-#read in dataset of all team conferences from data world
-confs = read.csv("https://query.data.world/s/nox5pnye7bjwbfcqa3ebudmie2f2dl"
-                 , header=TRUE, stringsAsFactors=FALSE)
-confs = data.table(confs)
-
-#filter out only relevant years and columns
-confs = confs[season >= "2015-16",c("school","conf","season")]
-
-#prepare odds data
-source("./prepare_odds_data.R")
-              
-#fuzzy join on just the unique names for efficiency
-score_teams = data.table(scores = unique(c(scores$Schl,scores$Opp)))
-conf_teams = data.table(confs = unique(confs$school))
-odds_teams = data.table(odds = unique(c(odds$Home,odds$Away)))
-join_tbl = stringdist_left_join(score_teams,conf_teams
-                           ,by = c(scores = "confs"),ignore_case = TRUE)
-join_tbl = stringdist_left_join(join_tbl,odds_teams
-                                ,by = c(scores = "odds"),ignore_case = TRUE)
-
-#write output of join_tbl to a csv so I can manually investigate and make any
-#necessary changes by hand
-write.csv(join_tbl,"./join_table.csv")
--- prepare_odds_data.R
+++ prepare_odds_data.R
@@ -1,8 +1,10 @@
+library(data.table)
+library(fuzzyjoin)
+
 ## prepare odds data
 
 # Prepare the odds data found on https://www.sportsbookreviewsonline.com/scoresoddsarchives/ncaabasketball/ncaabasketballoddsarchives.htm
 # to be usable for this analysis. 
-library(data.table)
 
 odds = data.table()
 for(year in c("2017-18","2016-17","2015-16")){
@@ -18,6 +20,10 @@ for(year in c("2017-18","2016-17","2015-16")){
 odds = odds[Close != "NL",]
 odds[Close == "pk",Close := "0"]
 odds[,Close := as.numeric(Close)]
+
+#fix a few naming inconsistencies
+odds[Team == "Denver",Team:="DenverU"]
+odds[Team == "Memphis",Team:="MemphisU"]
 
 # Separate home and away as 2 separate DF's
 V = odds[Rot%%2 == 1,]
@@ -42,3 +48,51 @@ odds[,Date := paste0(substr(Date,1,nchar(Date)-2),"/"
 
 odds[,Home_Spread := ifelse(Close < i.Close,-1*Close,i.Close)]
 odds = odds[complete.cases(odds),]
+
+write.csv(odds,"./data/odds.csv")
+
+
+## prepare scores data
+
+#read in the scores dataset from data world 
+scores = read.csv("https://query.data.world/s/ifvsddtb76qbf2i7f4ezmv6uutqtnj"
+                  ,header=TRUE ,stringsAsFactors=FALSE)
+scores = data.table(scores)
+
+#filter by strategy criteria
+scores = scores[!is.na(Rk..1) & is.na(Rk.) & Year >= "2015-16"
+                ,c("Year","Date","Schl","Rk.","Opp","Rk..1","PTS","OPP","MOV")]
+
+write.csv(scores,"./data/scores.csv")
+
+## prepare conference data
+
+#read in dataset of all team conferences from data world
+confs = read.csv("https://query.data.world/s/nox5pnye7bjwbfcqa3ebudmie2f2dl"
+                 , header=TRUE, stringsAsFactors=FALSE)
+confs = data.table(confs)
+
+#filter out only relevant years and columns
+confs = confs[season >= "2015-16",c("school","conf","season")]
+confs18 = confs[season == "2016-17",]
+confs18[,season := "2017-18"]
+confs = rbind(confs,confs18)
+
+write.csv(confs,"./data/confs.csv")
+
+
+## prepare the join table by fuzzy joining each unique list of names.
+## will manually adjust after writing to disk
+
+#fuzzy join on just the unique names for efficiency
+score_teams = data.table(scores = unique(c(scores$Schl,scores$Opp)))
+conf_teams = data.table(confs = unique(confs$school))
+odds_teams = data.table(odds = unique(c(odds$Home,odds$Away)))
+join_tbl = stringdist_left_join(score_teams,conf_teams
+                           ,by = c(scores = "confs"),ignore_case = TRUE)
+join_tbl = stringdist_left_join(join_tbl,odds_teams
+                                ,by = c(scores = "odds"),ignore_case = TRUE)
+
+#write output of join_tbl to a csv so I can manually investigate and make any
+#necessary changes by hand
+write.csv(join_tbl,"./data/join_table.csv")
