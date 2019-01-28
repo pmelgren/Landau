ddfba2c0e33939f65f2c889ad2cc4ad0381b0095
Pete Melgren <pmelgren@gmail.com>
2019-01-28 12:54:58 -0500

    Move join table out of the data folder and ignore everything under data
    Restructure landau.R to work off of the join table to bring together the 3 tables (confs, odds, scores) into one table that matches the criteria.
    
    Detele the 3 tables (they will be recreated but ignored

--- landau.R
+++ landau.R
@@ -3,9 +3,39 @@ library(data.table)
 
 # read in the 3 tables stored the data folder (after they have been prepared
 # by prepare_data.R)
-scores = data.table(read.csv("./scores.csv"))
-confs = data.table(read.csv("./confs.csv"))
-odds = data.table(read.csv("./odds.csv"))
-master = data.table(read.csv("./join_table.csv"))
+scores = data.table(read.csv("./data/scores.csv"))
+confs = data.table(read.csv("./data/confs.csv"))
+odds = data.table(read.csv("./data/odds.csv"))
+master = data.table(read.csv("./data/join_table.csv"))
 
+#merge the join table into scores
+setkey(master,scores)
+setkey(scores,Schl)
+scores = master[scores]
+setkey(scores,Opp)
+scores = master[scores]
+colnames(scores) = gsub("i.","home.",colnames(scores))
 
+#merge in conferences
+setkey(scores,confs,Year)
+setkey(confs,school,season)
+scores = confs[scores,nomatch = 0]
+ colnames(scores) = gsub("school","AwayTeam",colnames(scores))
+setkey(scores,home.confs,season)
+scores = confs[scores,nomatch = 0]
+
+#filter only in-conference games
+scores = scores[conference == i.conference,]
+
+#merge in odds
+setkey(scores,home.odds,Date)
+setkey(odds,Home,Date)
+scores = odds[scores]
+
+scores = scores[,list(Conference = conference,Date,Home,Away
+                      ,Away_Rank = Rk..1,Home_Spread,Home_Score = PTS
+                      ,Away_Score = OPP)]
+scores[,Cover := ifelse(Home_Score+Home_Spread > Away_Score,"Cover","No")]
+scores[Home_Score+Home_Spread == Away_Score,Cover := "Push"]
+
+write.csv(scores,"./Strategy_Results.csv",row.names = FALSE)
--- prepare_data.R
+++ prepare_data.R
@@ -45,11 +45,12 @@ odds[,Date := paste0(substr(Date,1,nchar(Date)-2),"/"
                      ,ifelse(nchar(Date) == 4,substr(Season,3,4)
                              ,substr(Season,6,7)))
      ]
+odds[,Date := gsub("/0","/",Date)]
 
 odds[,Home_Spread := ifelse(Close < i.Close,-1*Close,i.Close)]
 odds = odds[complete.cases(odds),]
 
-write.csv(odds,"./data/odds.csv")
+write.csv(odds,"./data/odds.csv",row.names = FALSE)
 
 
 ## prepare scores data
@@ -63,7 +64,7 @@ scores = data.table(scores)
 scores = scores[!is.na(Rk..1) & is.na(Rk.) & Year >= "2015-16"
                 ,c("Year","Date","Schl","Rk.","Opp","Rk..1","PTS","OPP","MOV")]
 
-write.csv(scores,"./data/scores.csv")
+write.csv(scores,"./data/scores.csv",row.names = FALSE)
 
 ## prepare conference data
 
@@ -71,14 +72,25 @@ write.csv(scores,"./data/scores.csv")
 confs = read.csv("https://query.data.world/s/nox5pnye7bjwbfcqa3ebudmie2f2dl"
                  , header=TRUE, stringsAsFactors=FALSE)
 confs = data.table(confs)
+colnames(confs) = gsub("conf","conference",colnames(confs))
 
 #filter out only relevant years and columns
-confs = confs[season >= "2015-16",c("school","conf","season")]
+confs = confs[season >= "2015-16",c("school","conference","season")]
+
+#add 2017-2018 to the dataset since it is missing
 confs18 = confs[season == "2016-17",]
 confs18[,season := "2017-18"]
 confs = rbind(confs,confs18)
 
-write.csv(confs,"./data/confs.csv")
+#manually handle any conference changes
+#see (https://en.wikipedia.org/wiki/NCAA_Division_I_conference_realignment#2017%E2%80%932018)
+confs[school == "wichita-state" & season == "2017-18",conference := "AAC"]
+confs[school == "valpraiso" & season == "2017-18",conference := "MVC"]
+
+#manually add in teams that are missing from the dataset
+#none to add right now as the only missing teams from this dataset don't match the criteria
+
+write.csv(confs,"./data/confs.csv",row.names = FALSE)
 
 
 ## prepare the join table by fuzzy joining each unique list of names.
@@ -95,4 +107,4 @@ join_tbl = stringdist_left_join(join_tbl,odds_teams
 
 #write output of join_tbl to a csv so I can manually investigate and make any
 #necessary changes by hand
-write.csv(join_tbl,"./data/join_table.csv")
+#write.csv(join_tbl,"./data/join_table.csv") #comment out to preserve the table
